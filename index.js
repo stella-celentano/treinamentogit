2a9a222fe483e7dc2a45d6644d0eda937750bfef
Andrew Gabriel Canassi Serati <andrewse2015@hotmail.com>
2019-08-27 21:27:24 -0300

    add Andrew, Stella, Stark and Vader

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('Hello World [ Hygor ]')
+console.log('Hello World [ Andrew ]')
+console.log('Hello World [ Stella ]')
+console.log('Hello World [ Stark ]')
+console.log('Hello World [ Vader ]')
